3d8d7d9377ae8d6b18e963b77777623ed86fb40f
Remi Boutteau <remi.boutteau@univ-rouen.fr>
2024-02-23 15:08:12 +0100

    ajout de bootstrap

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ let express = require('express')
 let app = express()
 let port = 3000
 
+app.use(express.static(__dirname + '/www' ) ) 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js' ))
+app.use( '/css' , express.static(__dirname + '/node_modules/bootstrap/dist/css' ))
+
 app.listen(port, ()=> {
     console.log('Le serveur est en route')
-    console.log(`Server listening at http://localhost:{port}`)
+    console.log(`Server listening at http://localhost:${port}`)
 })
 
 app.get('/', (req, res, next) => {
-    res.send('Bonjour')
+    res.sendFile('/www/index.html')
 })
