2e12e492668ee4d5d45b8c2ae5221b60ad18204a
Remi Boutteau <remi.boutteau@univ-rouen.fr>
2024-02-23 15:43:28 +0100

    Ajout du moteur ejs

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ let express = require('express')
 let app = express()
 let port = 3000
 
+app.set('view engine', 'ejs')
+
 app.use(express.static(__dirname + '/www' ) ) 
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js' ))
 app.use( '/css' , express.static(__dirname + '/node_modules/bootstrap/dist/css' ))
+app.use('/views', express.static(__dirname+'/views'))
 
 app.listen(port, ()=> {
     console.log('Le serveur est en route')
@@ -12,5 +15,9 @@ app.listen(port, ()=> {
 })
 
 app.get('/', (req, res, next) => {
-    res.sendFile('/www/index.html')
+    res.render('index.ejs')
+})
+
+app.get('/page2', (req, res, next) => {
+    res.render('page2.ejs')
 })
