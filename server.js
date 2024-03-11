e24d47114b9a8860ced5b25fe6492db51aa5a93d
Remi Boutteau <remi.boutteau@univ-rouen.fr>
2024-03-11 13:40:24 +0100

    ajout du formulaire

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require('express')
+let bodyparser = require('body-parser')
 let app = express()
 let port = 3000
 
@@ -8,6 +9,9 @@ app.use(express.static(__dirname + '/www' ) )
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js' ))
 app.use( '/css' , express.static(__dirname + '/node_modules/bootstrap/dist/css' ))
 app.use('/views', express.static(__dirname+'/views'))
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 
 let myobject = {
     nom: "mon objet",
@@ -25,4 +29,9 @@ app.get('/', (req, res, next) => {
 
 app.get('/page2', (req, res, next) => {
     res.render('page2.ejs')
+})
+
+app.post('/page2', (req, res, next) => {
+    console.log(req.body.name)
+    res.redirect('/')
 })
