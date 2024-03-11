6bd6833d8d52e6fb388d2608dd40dcaa993ce864
Remi Boutteau <remi.boutteau@univ-rouen.fr>
2024-03-11 13:20:11 +0100

    ajout paramètres

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js' ))
 app.use( '/css' , express.static(__dirname + '/node_modules/bootstrap/dist/css' ))
 app.use('/views', express.static(__dirname+'/views'))
 
+let myobject = {
+    nom: "mon objet",
+    valeur: 10
+}
+
 app.listen(port, ()=> {
     console.log('Le serveur est en route')
     console.log(`Server listening at http://localhost:${port}`)
 })
 
 app.get('/', (req, res, next) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {monobjet: myobject})
 })
 
 app.get('/page2', (req, res, next) => {
